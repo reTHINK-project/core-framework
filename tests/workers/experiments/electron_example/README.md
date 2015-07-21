31750808e8be7a473e655002ecb7f972b24eeed3
joelpinheiro <joelpinheiro@aluno-1036.wireless.ua.pt>
2015-07-21 17:53:42 +0100

    Electron experiment

---
+++ tests/workers/experiments/electron_example/README.md
@@ -0,0 +1,16 @@
+### Electron experiment
+
+[Electron](electron.atom.io) application that runs with system tray and use chromium WebRTC to establish video.
+
+![screenshot.png](img/screenshot.png)
+
+### How it works
+
+The app itself is written in JS, HTML and CSS.
+
+##### Developing
+
+```bash
+npm install # installs experiments environment and all the deps
+npm start # runs the experiments
+```
---
+++ tests/workers/experiments/electron_example/main.js
@@ -0,0 +1,53 @@
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 350, height: 700});
+
+  // and load the index.html of the app.
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  // Open the devtools.
+  //mainWindow.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
+
+
+var app = require('app');
+var Menu = require('menu');
+var Tray = require('tray');
+
+var appIcon = null;
+app.on('ready', function(){
+  appIcon = new Tray('/Users/joelpinheiro/Downloads/icon.png');
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'reTHINK test', type: 'radio', checked: true },
+    { label: 'Open WebRTC', type: 'radio' }
+  ]);
+  appIcon.setToolTip('This is my application.');
+  appIcon.setContextMenu(contextMenu);
+});
