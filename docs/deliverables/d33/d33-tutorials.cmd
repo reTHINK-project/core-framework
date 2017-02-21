642166bf99a5f0be6da1945350045f37338d516d
pchainho <paulo-g-chainho@ptinovacao.pt>
2017-02-21 15:49:23 +0000

    D3.3 preparation

---
+++ docs/deliverables/d33/d33-tutorials.cmd
@@ -0,0 +1,23 @@
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\messaging-framework\readme.md -o D3.3-tutorials-messaging-framework-overview.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\messaging-framework\protofly.md -o D3.3-tutorials-protofly.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\messaging-framework\msg-node-design.md -o D3.3-tutorials-msg-node-design.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\messaging-framework\stub-specification.md -o D3.3-tutorials-stub-specification.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\messaging-framework\p2p-data-sync.md -o d3.3-tutorials-p2p-data-sync.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\tutorials\hyperty.md -o d3.3-tutorials-hyperty.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\trust-management\readme.md -o d3.3-tutorials-hyperty-trust.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\trust-management\idp-proxy-development.md -o d3.3-tutorials-idp-proxy.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\tutorials\development-of-hyperties.md -o d3.3-tutorials-development-of-hyperties.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\tutorials\development-of-apps.md -o d3.3-tutorials-development-of-apps.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\startup\readme.md -o d3.3-tutorials-startup.docx
+
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\legacy-interworking/interworking-stubs-development.md -o d3.3-tutorials-interworking.docx
