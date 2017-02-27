0b1767bcc7b83d90965496c52eec9a0de7ac2d8c
pchainho <paulo-g-chainho@ptinovacao.pt>
2017-02-27 13:04:27 +0000

    Today telco added

--- docs/deliverables/d33/d33-tutorials.cmd
+++ docs/deliverables/d33/d33-tutorials.cmd
@@ -12,8 +12,6 @@ pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\re
 
 pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\trust-management\readme.md -o d3.3-tutorials-hyperty-trust.docx
 
-pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\trust-management\idp-proxy-development.md -o d3.3-tutorials-idp-proxy.docx
-
 pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\tutorials\development-of-hyperties.md -o d3.3-tutorials-development-of-hyperties.docx
 
 pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. C:\Projectos\reTHINK\WP3\git\specs\tutorials\development-of-apps.md -o d3.3-tutorials-development-of-apps.docx
